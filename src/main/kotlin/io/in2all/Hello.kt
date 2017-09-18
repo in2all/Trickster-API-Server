8f7b7d693dadc1a888316af69a4aef68ea242bbf
misonijnik <misonijnik@gmail.com>
2017-09-19 02:15:12 +0300

    Now "Hello World" on Vert.x!

---
+++ src/main/kotlin/io/in2all/Hello.kt
@@ -0,0 +1,20 @@
+package io.in2all
+
+import io.vertx.core.Vertx
+
+fun main(args: Array<String>) {
+    val vertx = Vertx.vertx()
+    val server = vertx.createHttpServer()
+
+    server.requestHandler({ request ->
+
+        // This handler gets called for each request that arrives on the server
+        val response = request.response()
+        response.putHeader("content-type", "text/plain")
+
+        // Write to the response and end it
+        response.end("Hello World!")
+    })
+
+    server.listen(8080)
+}
---
+++ src/test/kotlin/io/in2all/HelloTest.kt
@@ -0,0 +1,8 @@
+package io.in2all
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
