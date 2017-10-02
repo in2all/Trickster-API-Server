f91ab304f0805de7b1c3d5b96253207c74b6403d
Vlad <weststalkers@gmail.com>
2017-10-02 21:55:44 +0300

    Added signup page and TODO filtres.

--- src/main/kotlin/co/in2all/trickster/api/server/repositories/UsersRepository.kt
+++ src/main/kotlin/co/in2all/trickster/api/server/repositories/UsersRepository.kt
@@ -6,7 +6,14 @@ import org.springframework.data.neo4j.repository.GraphRepository
 import org.springframework.data.repository.query.Param
 
 interface UsersRepository : GraphRepository<User>{
+    @Query("MATCH (u:User {email: {email}}) RETURN u")
+    fun get(@Param("email") email: String): User?
+
     @Query("MATCH (u:User {email: {email}, password: {password}}) RETURN u")
     fun get(@Param("email") email: String,
             @Param("password") password: String): User?
+
+    @Query("CREATE (:User {email: {email}, password: {password}})")
+    fun create(@Param("email") email: String,
+               @Param("password") password: String)
 }
