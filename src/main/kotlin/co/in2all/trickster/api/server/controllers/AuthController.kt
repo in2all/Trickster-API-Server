101a76ed869c8b9c5c27d29291f153c3ca9184dd
Vlad <weststalkers@gmail.com>
2017-10-02 21:55:44 +0300

    Added signup page and TODO filtres.

--- src/main/kotlin/co/in2all/trickster/api/server/controllers/AuthController.kt
+++ src/main/kotlin/co/in2all/trickster/api/server/controllers/AuthController.kt
@@ -24,10 +24,8 @@ class AuthController @Autowired constructor(
 
     @GetMapping("/signin/{client_id}")
     fun signInWithApp(@PathVariable(value = "client_id") clientId: String): ModelAndView {
-        val app = appsRepository.get(clientId)
-
-        return if (app != null) {
-            ModelAndView("signin", hashMapOf("client_id" to app.client_id))
+        return if (appsRepository.get(clientId) != null) {
+            ModelAndView("signin", hashMapOf("client_id" to clientId))
         } else {
             ModelAndView("404", HttpStatus.NOT_FOUND)
         }
@@ -39,11 +37,13 @@ class AuthController @Autowired constructor(
             @RequestParam(value = "email") email: String,
             @RequestParam(value = "password") password: String): String {
         val app = appsRepository.get(clientId)
+        // TODO-misonijnik: Тут проверяется существование юзера с переданным паролем. Добавить шифрование.
         val user = usersRepository.get(email, password)
 
         // TODO: Сделать что-нибудь с мутабельностью полей user!
         return if (app != null && user != null && user.email != null) {
             val token = Safeguard.getUniqueToken()
+            // TODO: Перенести время expiresIn в конфиги.
             val expiresIn = Date().time + TimeUnit.MINUTES.toMillis(10)
 
             authTokensRepository.create(user.email!!, app.client_id!!, token, expiresIn)
@@ -53,6 +53,46 @@ class AuthController @Autowired constructor(
             // TODO: Сообщение о неверном логине или пароле и просьба повторить их ввод.
             // Сделать после появления дизайна страницы.
             "redirect:/signin/$clientId"
+        }
+    }
+
+    @GetMapping("/signup/{client_id}")
+    fun signUpWithApp(
+            @PathVariable(value = "client_id") clientId: String): ModelAndView {
+        return if (appsRepository.get(clientId) != null) {
+            ModelAndView("signup", hashMapOf("client_id" to clientId))
+        } else {
+            ModelAndView("404", HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @PostMapping("/signup/{client_id}")
+    fun createUserAndAccessTokenAndRedirect(
+            @PathVariable(value = "client_id") clientId: String,
+            @RequestParam(value = "email") email: String,
+            @RequestParam(value = "password") password: String): String {
+        val app = appsRepository.get(clientId)
+
+        return if (app != null) {
+            return if (usersRepository.get(email) == null) {
+                val token = Safeguard.getUniqueToken()
+                // TODO: Перенести время expiresIn в конфиги.
+                val expiresIn = Date().time + TimeUnit.MINUTES.toMillis(10)
+
+                // TODO-misonijnik: Добавить шифрование пароля перед записью в БД.
+                usersRepository.create(email, password)
+                authTokensRepository.create(email, clientId, token, expiresIn)
+
+                "redirect:${app.redirect_uri}/$token"
+            } else {
+                // TODO: Сообщение о том, что пользователь уже существует.
+                // Сделать после появления дизайна страницы.
+                "redirect:/signup/$clientId"
+            }
+        } else {
+            // TODO: Сообщение о неверном логине или пароле и просьба повторить их ввод.
+            // Сделать после появления дизайна страницы.
+            "redirect:/signup/$clientId"
         }
     }
 
@@ -82,6 +122,7 @@ class AuthController @Autowired constructor(
             @RequestParam(value = "client_secret") clientSecret: String): Any {
         return if (sessionsRepository.get(refreshToken, clientId, clientSecret) != null) {
             val newAccessToken = Safeguard.getUniqueToken()
+            // TODO: Перенести время expiresIn в конфиги.
             val newExpiresIn = Date().time + TimeUnit.DAYS.toMillis(1)
             val newRefreshToken = Safeguard.getUniqueToken()
 
