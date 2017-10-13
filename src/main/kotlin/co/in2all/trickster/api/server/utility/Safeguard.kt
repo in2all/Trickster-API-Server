c8c2dc42d0201c29015eb8f9f6229f810314e3e2
misonijnik <misonijnik@gmail.com>
2017-10-13 13:32:16 +0300

    Added encryption functions in Safeguard.kt

--- src/main/kotlin/co/in2all/trickster/api/server/utility/Safeguard.kt
+++ src/main/kotlin/co/in2all/trickster/api/server/utility/Safeguard.kt
@@ -1,7 +1,11 @@
 package co.in2all.trickster.api.server.utility
-
+import com.typesafe.config.ConfigFactory
+import org.springframework.security.crypto.password.StandardPasswordEncoder
 import java.util.UUID
 
 object Safeguard {
+    private val encoder = StandardPasswordEncoder(ConfigFactory.load().getString("security.secret"))
     fun getUniqueToken() = UUID.randomUUID().toString().split("-").joinToString("")
+    fun encodePassword(password: String) = encoder.encode(password)
+    fun assertPassword(password: String, encodedPassword : String) = encoder.matches(password, encodedPassword)
 }
